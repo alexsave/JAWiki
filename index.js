1e213c8162288032fb0f98223bad5c00ae6dcf7f
Alex Saveliev <alexvsaveliev@gmail.com>
2020-05-29 20:05:18 -0400

    implemented a cache system

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const https = require('https');
+const fs = require('fs');
 
 const baseURL = 'https://ja.wikipedia.org/wiki/';
 const jjba = 'ジョジョの奇妙な冒険';
 
-const buffer = [];
-https.get(baseURL+encodeURIComponent(jjba), (res) => {
-    console.log(res.statusCode);
-    res.on('data', d => {
-        buffer.append(d);
-        //console.log(d.toString());
-        //console.log();
-    });
+const page = jjba;
 
-    res.on('end', () => {
-
-    });
+const loadPage = (page,cb) => {
+    const filename = `cache/${page}.html`;
+    if(fs.existsSync(filename))
+        fs.readFile(filename, (err, data) => cb(data));
+        //cb(fs.readFileSync(filename));
 
 
-}).on('error', e => console.log(e));
+    const buffer = [];
+    https.get(baseURL+encodeURIComponent(page), (res) => {
+        console.log(res.statusCode);
+        res.on('data', d => {
+            buffer.push(d);
+        });
+
+        res.on('end', () => {
+            fs.writeFile(filename, buffer, () => cb(buffer));
+        });
+    }).on('error', e => console.log(e));
+};
+
+loadPage(jjba, buff => {
+    console.log(buff.toString());
+});
